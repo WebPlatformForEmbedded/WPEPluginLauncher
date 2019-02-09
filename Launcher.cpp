a810689184b2aebae91f21a5c6f6f742d581ae19
Pierre Wielders <pierre@wielders.net>
2019-02-09 15:37:33 +0100

    [LAUNCHER] Add verbosity.            Do not start the plugin if the timing parmeters are NOT correct.            Closed a Shutdown race condition.

--- Launcher.cpp
+++ Launcher.cpp
@@ -19,91 +19,34 @@ SERVICE_REGISTRATION(Launcher, 1, 0);
 
 /* virtual */ const string Launcher::Initialize(PluginHost::IShell* service)
 {
-    Time time;
+    Core::Time scheduleTime;
     Time interval;
-    mode timeMode = RELATIVE;
     string message;
     Config config;
 
     ASSERT(_service == nullptr);
     ASSERT(_memory == nullptr);
+    ASSERT(_activity.IsValid() == false);
 
     // Setup skip URL for right offset.
     _service = service;
 
     config.FromString(_service->ConfigLine());
 
-    if ((config.Command.IsSet() == true) && (config.Command.Value().empty() == false)) {
-        _closeTime = (config.CloseTime.Value());
-
-        if (config.ScheduleTime.IsSet() == true) {
-
-            timeMode = config.ScheduleTime.Mode.Value();
-
-            time = Time(config.ScheduleTime.Time.Value());
-            if (time.IsValid() != true) {
-                SYSLOG(Trace::Fatal, (_T("Time format is wrong")));
-            }
-
-            interval = Time(config.ScheduleTime.Interval.Value());
-            if (interval.IsValid() != true) {
-                SYSLOG(Trace::Fatal, (_T("Interval format is wrong")));
-            }
-        }
-
+    if ((config.Command.IsSet() == false) || (config.Command.Value().empty() == true)) {
+        message = _T("Command is not set");
+    }
+    else if (ScheduleParameters(config, message, scheduleTime, interval) == true) {
         _memory = Core::Service<MemoryObserverImpl>::Create<Exchange::IMemory>(0);
         ASSERT(_memory != nullptr);
 
         _activity = Core::ProxyType<Job>::Create(&config, interval, _memory);
-        if (_activity.IsValid() == true) {
-            if (_activity->IsOperational() == false) {
-                // Well if we where able to parse the parameters (if needed) we are ready to start it..
-                _observer.Register(&_notification);
-                Core::Time scheduledTime;
-                if (time.IsValid() == true) {
-                    if (timeMode == RELATIVE) { //Schedule Job at relative timing
-                        scheduledTime = Core::Time::Now();
+        ASSERT (_activity.IsValid() == true);
 
-                        uint64_t timeValueToTrigger = (((time.HasHours() ? time.Hours(): 0) * MinutesPerHour +
-                                                        (time.HasMinutes() ? time.Minutes(): 0)) * SecondsPerMinute + time.Seconds()) * MilliSecondsPerSecond;
-                        scheduledTime.Add(timeValueToTrigger);
+        // Well if we where able to parse the parameters (if needed) we are ready to start it..
+        _observer.Register(&_notification);
 
-                    }
-                    else {
-                        //Schedule Job at absolute timing
-                        if (timeMode == ABSOLUTE_WITH_INTERVAL) {
-                            scheduledTime = FindAbsoluteTimeForSchedule(time, interval);
-                        }
-                        else {
-                            scheduledTime = FindAbsoluteTimeForSchedule(time, Time());
-                        }
-                    }
-                    PluginHost::WorkerPool::Instance().Schedule(scheduledTime, _activity);
-                }
-                else {
-                    PluginHost::WorkerPool::Instance().Submit(_activity);
-                }
-
-            }
-            else {
-                _activity.Release();
-                message = _T("Could not parse the configuration for the job.");
-            }
-        }
-        else {
-            message = _T("Could not create the job.");
-        }
-    }
-    else {
-        message = _T("Command is not set");
-        SYSLOG(Trace::Fatal, (message.c_str()));
-        TRACE_L1(message.c_str());
-    }
-    if (_activity.IsValid() == false) {
-        if (_memory != nullptr) {
-            _memory->Release();
-            _memory = nullptr;
-        }
+        _activity->Schedule(scheduleTime);
     }
 
     return (message);
@@ -113,26 +56,16 @@ SERVICE_REGISTRATION(Launcher, 1, 0);
 {
     ASSERT(_service == service);
     ASSERT(_memory != nullptr);
+    ASSERT(_acitivity.IsValid() == true);
 
-    PluginHost::WorkerPool::Instance().Revoke(_activity);
-
+    _memory->Observe(0);
     _observer.Unregister(&_notification);
 
-    if (_memory != nullptr) {
-        _memory->Release();
-        _memory = nullptr;
-    }
-    if (_activity->Process().IsActive() == true) {
-        // First try a gentle touch....
-        _activity->Process().Kill(false);
+    _activity->Shutdown();
+    _activity.Release();
 
-        // Wait for a maximum of 3 Seconds before we shoot the process!!
-        if (_activity->Process().WaitProcessCompleted(_closeTime * 1000) != Core::ERROR_NONE) {
-            _activity->Process().Kill(true);
-            _activity->Process().WaitProcessCompleted(_closeTime * 1000);
-        }
-    }
-
+    _memory->Release();
+    _memory = nullptr;
     _service = nullptr;
 }
 
@@ -144,71 +77,93 @@ SERVICE_REGISTRATION(Launcher, 1, 0);
 
 void Launcher::Update(const ProcessObserver::Info& info)
 {
+    // There is always an Activity as the unregister takes place in a locked sequence. So no new notifications
+    // Will enter after the unregister of this handler.
+    ASSERT (_activity.IsValid() == true);
+    ASSERT(_service != nullptr);
+
     // This can potentially be called on a socket thread, so the deactivation (wich in turn kills this object) must be done
     // on a seperate thread. Also make sure this call-stack can be unwound before we are totally destructed.
-    if ((_activity.IsValid() == true) && (_activity->Pid() == info.Id())) {
+    if ( (_activity->Pid() == info.Id()) && (info.Event() == ProcessObserver::Info::EVENT_EXIT) ) { 
 
-        ASSERT(_service != nullptr);
+        _memory->Observe(0);
+        uint32_t result = _activity->ExitCode();
 
-        if (info.Event() == ProcessObserver::Info::EVENT_EXIT) {
-
-            _memory->Observe(0);
-            if ((info.ExitCode() & 0xFFFF) == 0) {
-                // Only do this if we do not need a retrigger on an intervall.
-                if (_activity->IsOperational() == false) {
-                    PluginHost::WorkerPool::Instance().Submit(PluginHost::IShell::Job::Create(_service, PluginHost::IShell::DEACTIVATED, PluginHost::IShell::AUTOMATIC));
-                }
-                else {
-                    TRACE(Trace::Information, (_T("The process has run, and completed succefully.")));
-                }
-            }
-            else {
-                PluginHost::WorkerPool::Instance().Submit(PluginHost::IShell::Job::Create(_service, PluginHost::IShell::DEACTIVATED, PluginHost::IShell::FAILURE));
-            }
+        if (result != Core::ERROR_NONE) {
+            SYSLOG(Trace::Fatal, (_T("FORCED Shutdown: %s by error: %d."), _service->Callsign().c_str(), result));
+            PluginHost::WorkerPool::Instance().Submit(PluginHost::IShell::Job::Create(_service, PluginHost::IShell::DEACTIVATED, PluginHost::IShell::FAILURE));
         }
-    }
-}
-
-Core::Time Launcher::FindAbsoluteTimeForSchedule(const Time& absoluteTime, const Time& interval) {
-    Core::Time startTime = Core::Time::Now();
-    // Go to a first viable start time (compared to the current time, in seconds)
-    Core::Time slotTime = Core::Time(startTime.Year(), startTime.Month(), startTime.Day(),
-                                   (absoluteTime.HasHours()   ? absoluteTime.Hours() : startTime.Hours()),
-                                   (absoluteTime.HasMinutes() ? absoluteTime.Minutes() : startTime.Minutes()),
-                                   absoluteTime.Seconds(), 0, false);
-
-    if (interval.IsValid() == false) {
-        if (slotTime < startTime) {
-            uint32_t jump ((absoluteTime.HasHours() ? HoursPerDay * MinutesPerHour * SecondsPerMinute :
-                           (absoluteTime.HasMinutes() ? MinutesPerHour * SecondsPerMinute : SecondsPerMinute)) * MilliSecondsPerSecond);
-
-            slotTime.Add(jump);
-        }
-    }
-    else {
-        uint32_t intervalJump = ( (interval.HasHours() ? interval.Hours() * MinutesPerHour * SecondsPerMinute : 0) +
-                                  (interval.HasMinutes() ? interval.Minutes() * SecondsPerMinute : 0) +
-                                   interval.Seconds() ) * MilliSecondsPerSecond;
-
-        ASSERT (intervalJump != 0);
-        if (slotTime >= startTime) {
-            Core::Time workTime (slotTime);
-
-            while (workTime.Sub(intervalJump) > startTime) {
-                slotTime.Sub(intervalJump);
-            }
+        else if (_activity->Continuous() == false) {
+            TRACE(Trace::Information, (_T("Launcher [%s] has run succesfully, deactivation requested."), _service->Callsign().c_str()));
+            PluginHost::WorkerPool::Instance().Submit(PluginHost::IShell::Job::Create(_service, PluginHost::IShell::DEACTIVATED, PluginHost::IShell::AUTOMATIC));
         }
         else {
-            // Now increment with the intervall till we reach a valid point
-            while (slotTime < startTime) {
-                slotTime.Add(intervalJump);
+            TRACE(Trace::Information, (_T("Launcher [%s] has run succesfully, scheduled for the next run."), _service->Callsign().c_str()));
+        }
+    }
+}
+
+bool Launcher::ScheduleParameters(const Config& config, string& message, Core::Time& scheduleTime, Time& interval) {
+
+    // initialize with defaults..
+    scheduleTime = Core::Time::Now();
+    interval = Time();
+
+    // Only if a schedule section is set, we need to do something..
+    if (config.ScheduleTime.IsSet() == true) {
+
+        mode timeMode = config.ScheduleTime.Mode.Value();
+        Time time(config.ScheduleTime.Time.Value());
+
+        interval = Time(config.ScheduleTime.Interval.Value());
+
+        if (time.IsValid() != true) {
+            message = _T("Incorrect time format for Scheduled time.");
+        }
+        else if ( (config.ScheduleTime.Interval.IsSet() == true) && (interval.IsValid() != true) ) {
+            message = _T("Incorrect time format for Interval time.");
+        }
+        else if ( (timeMode == ABSOLUTE_WITH_INTERVAL) && ((interval.IsValid() == false) || (interval.TimeInSeconds() == 0)) ) {
+            message = _T("Requested mode is ABSOLUTE WITH INTERVAL but no interval (or 0 second interval) is given.");
+        }
+        else {
+            // All signals green, we have valid input, calculate the ScheduleTime/Interval time
+            if (timeMode == RELATIVE) { //Schedule Job at relative timing
+                scheduleTime.Add(time.TimeInSeconds() * Time::MilliSecondsPerSecond);
+            }
+            else {
+                Core::Time now (scheduleTime);
+                // Go to a first viable start time (compared to the current time, in seconds)
+                scheduleTime = Core::Time(now.Year(), now.Month(), now.Day(),
+                                             (time.HasHours()   ? time.Hours()   : now.Hours()),
+                                             (time.HasMinutes() ? time.Minutes() : now.Minutes()),
+                                              time.Seconds(), 0, false);
+
+                if (timeMode == ABSOLUTE_WITH_INTERVAL) {
+                    uint32_t intervalJump = interval.TimeInSeconds() * Time::MilliSecondsPerSecond;
+
+                    if (scheduleTime >= now) {
+                        Core::Time workTime (scheduleTime);
+
+                        while (workTime.Sub(intervalJump) > now) { scheduleTime.Sub(intervalJump); }
+                    }
+                    else {
+                        // Now increment with the interval till we reach a valid point
+                        while (scheduleTime < now) { scheduleTime.Add(intervalJump); }
+                    }
+                }
+                else if (scheduleTime < now) {
+                    uint32_t jump = (time.HasHours()   ? Time::HoursPerDay * Time::MinutesPerHour * Time::SecondsPerMinute :
+                                    (time.HasMinutes() ? Time::MinutesPerHour * Time::SecondsPerMinute : Time::SecondsPerMinute));
+
+                    scheduleTime.Add(jump * Time::MilliSecondsPerSecond);
+                }
             }
         }
     }
-
-    return slotTime;
+    return (message.empty());
 }
-
+ 
 } //namespace Plugin
 
 } // namespace WPEFramework
